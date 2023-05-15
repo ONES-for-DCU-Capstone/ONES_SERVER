35850b66e536034d7e59a83397e61ce93cca4875
jm <hijno@naver.com>
2023-05-15 14:01:26 +0900

    7. post view_ timestamp

--- app/src/main/java/com/example/ones_02/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/example/ones_02/navigation/DetailViewFragment.kt
@@ -2,20 +2,22 @@ package com.example.ones_02.navigation
 
 import android.app.ActivityOptions
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
-import android.util.Log
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
+import android.widget.LinearLayout
+import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.ones_02.R
 import com.example.ones_02.navigation.model.ContentDTO
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
@@ -23,6 +25,12 @@ import kotlinx.android.synthetic.main.fragment_detail.*
 import kotlinx.android.synthetic.main.fragment_detail.view.*
 import kotlinx.android.synthetic.main.fragment_postview.view.*
 import kotlinx.android.synthetic.main.item_detail.view.*
+import java.sql.Date
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.Collections.max
+import kotlin.collections.ArrayList
+import kotlin.math.max
 
 class DetailViewFragment : Fragment() {
     var firestore : FirebaseFirestore?= null
@@ -37,9 +45,9 @@ class DetailViewFragment : Fragment() {
         firestore = FirebaseFirestore.getInstance()
 
         uid = FirebaseAuth.getInstance().currentUser?.uid
-
-        var contentDTOs : ArrayList<ContentDTO> = arrayListOf()
-        var contentUidList : ArrayList<String> = arrayListOf()
+//
+//        var contentDTOs : ArrayList<ContentDTO> = arrayListOf()
+//        var contentUidList : ArrayList<String> = arrayListOf()
 
         view.detailview_recyclerveiw.adapter = DetailViewRecyclerViewAdapter()
         view.detailview_recyclerveiw.layoutManager = LinearLayoutManager(activity)
@@ -57,10 +65,14 @@ class DetailViewFragment : Fragment() {
                 contentDTOs.clear()
                 contentUidList.clear()
 
-
                 if(querySnapshot == null) return@addSnapshotListener
                 for(snapshot in querySnapshot!!.documents){
                     var item = snapshot.toObject(ContentDTO::class.java)
+//                    val item = snapshot.toObject(ContentDTO::class.java)?.apply {
+//                        this.timestamp = timestamp?.let { Timestamp(it) }
+//                    } ?: ContentDTO() // 만약 snapshot.toObject(ContentDTO::class.java)가 null이라면 새로운 ContentDTO 객체 생성
+
+
                     contentDTOs.add(item!!)
                     contentUidList.add(snapshot.id)
                 }
@@ -79,7 +91,6 @@ class DetailViewFragment : Fragment() {
             //서버에서 넘어오는 데이터를 매핑시켜줌
             var viewholder = (p0 as CustomViewHolder).itemView
 
-
             //UserId
 //            viewholder.profile_textview.text = contentDTOs!![p1].userId
 
@@ -88,6 +99,100 @@ class DetailViewFragment : Fragment() {
 
             //title
             viewholder.post_feed_product_title.text = contentDTOs!![p1].title
+
+//            val sdf = SimpleDateFormat("yyyy년-MM월-dd일")
+//            val date = Date(contentDTOs!![p1].timestamp)
+//            viewholder.post_feed_product_time.text = sdf.format(date)
+//            viewholder.post_feed_product_time.text = contentDTOs!![p1].timestamp?.time.toString()
+
+//            viewholder.post_feed_product_time.text = contentDTOs!![p1].timestamp?.toDate()?.toString()
+
+            val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
+            val date = contentDTOs!![p1].timestamp?.toDate()
+            viewholder.post_feed_product_time.text = dateFormat.format(date)
+
+//            viewholder.post_feed_product_time.text = contentDTOs!![p1].timestamp.toString()
+
+
+//            val tagbutton = viewholder.post_feed_product_tag as AppCompatButton
+//            tagbutton.setText(contentDTOs!![p1].tag)
+
+
+//            val TAG_BUTTON_PADDING = 10
+//            val TAG_BUTTON_WIDTH_BASE = 10
+//
+//            val tagbutton = viewholder.post_feed_product_tag as AppCompatButton
+//            val tag = contentDTOs!![p1].tag
+//            val paint = tagbutton.paint
+//            val tagWidth = paint.measureText(tag) + TAG_BUTTON_PADDING * 2
+//            tagbutton.width = max(tagWidth.toInt(), TAG_BUTTON_WIDTH_BASE)
+//            tagbutton.text = tag
+
+            val tagLayout = viewholder.post_feed_product_tag_layout
+            val tags = contentDTOs!![p1].tag?.split(",")
+
+// 기존에 있던 버튼들 삭제
+            tagLayout.removeAllViews()
+
+// 각 태그마다 버튼 생성
+            if (tags != null) {
+                for (tag in tags) {
+                    val tagButton = AppCompatButton(context!!)
+
+                    // 버튼의 글자 크기 측정
+                    val paint = tagButton.paint
+                    val textWidth = paint.measureText(tag)
+
+                    // 버튼의 크기 동적으로 설정
+                    val params = LinearLayout.LayoutParams(
+                        textWidth.toInt() + 40,  // 버튼의 크기 = 글자의 길이 + 여백
+                        78
+                    )
+                    params.setMargins(0, 0, 10, 0) // left, top, right, bottom
+                    tagButton.layoutParams = params
+
+                    tagButton.text = tag
+                    tagButton.setBackgroundResource(R.drawable.shape_tagbox)
+                    tagButton.setTextColor(Color.WHITE)
+                    tagLayout.addView(tagButton)
+                }
+            }
+
+
+
+
+//            val tagLayout = viewholder.post_feed_product_tag_layout
+//            val tags = contentDTOs!![p1].tag?.split(",")
+//
+//            // 기존에 있던 버튼들 삭제
+//            tagLayout.removeAllViews()
+//
+//            // 각 태그마다 버튼 생성
+//            if (tags != null) {
+//                for (tag in tags) {
+//                    val tagButton = AppCompatButton(context!!)
+//                    tagButton.layoutParams = LinearLayout.LayoutParams(
+//                        LinearLayout.LayoutParams.WRAP_CONTENT,
+//                        LinearLayout.LayoutParams.WRAP_CONTENT
+//                    )
+//                    tagButton.text = tag.toString()
+//                    tagButton.setBackgroundResource(R.drawable.shape_tagbox)
+//                    tagButton.setTextColor(Color.WHITE)
+//                    tagLayout.addView(tagButton)
+//                }
+//            }
+
+//            val tagButton = viewholder.post_feed_product_tag as AppCompatButton
+//            val tags = contentDTOs!![p1].tag
+//            for (tag in tags) {
+//                val tagView = LayoutInflater.from(context).inflate(R.layout.item_detail, null)
+//                val tagTextView = tagView.findViewById<TextView>(R.id.post_feed_product_tag_text)
+//                tagTextView.text = tag
+//                viewholder.post_feed_product_tag_layout.addView(tagView)
+//            }
+
+
+
 
             viewholder.post_feed_product_price.text = contentDTOs!![p1].price
 
@@ -114,14 +219,10 @@ class DetailViewFragment : Fragment() {
 //            }
 
 
-
-
             post_feed_bnt_post_wrt.setOnClickListener{
                 val intent = Intent(p0.itemView.context, AddPhotoActivity::class.java)
                 p0.itemView.context.startActivity(intent)
             }
-
-
 
             p0.itemView.setOnClickListener(View.OnClickListener { view ->
                 firestore?.collection("images")?.get()?.addOnSuccessListener { querySnapshot ->
@@ -133,6 +234,7 @@ class DetailViewFragment : Fragment() {
 
                 val intent = Intent(view.context, PostViewFragment::class.java)
                 intent.putExtra("documentId", contentDTOs[p1].id)
+                intent.putExtra("destinationUid", contentDTOs[p1].uid)
 
                 var activityOption: ActivityOptions? = null
 
@@ -166,31 +268,31 @@ class DetailViewFragment : Fragment() {
 //            }
 
             //p0.itemView = chat_btn  <- p0.itemView는 전체 테이블을 의미
-            viewholder.chat_btt.setOnClickListener(View.OnClickListener { view ->
-                val intent = Intent(view.context, ChatRoomActivity::class.java)
-                intent.putExtra("destinationUid", contentDTOs[p1].uid)
-                var activityOptions: ActivityOptions? = null
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                    activityOptions = ActivityOptions.makeCustomAnimation(
-                        view.context,
-                        R.anim.fromright,
-                        R.anim.toleft
-                    )
-                    Log.d("uid", "$uid")
-                    Log.d("ui2", "${contentDTOs[p1].uid}")
-
-                    startActivity(intent, activityOptions.toBundle())
-
-                    //당근 마켓 처럼 자신의 게시글인 경우에는 채팅대화방 보기 버튼을 하고 싶은데.. 그건 아직..
-//                    if( uid == contentDTOs[p1].uid ){
-//                        Snackbar.make(view, "본인이 등록한 아이템입니다.", Snackbar.LENGTH_LONG).show()
-//                    }
-//                    else{
-//                        startActivity(intent, activityOptions.toBundle())
-//                    }
-                }
-            })
+//            viewholder.chat_btt.setOnClickListener(View.OnClickListener { view ->
+//                val intent = Intent(view.context, ChatRoomActivity::class.java)
+//                intent.putExtra("destinationUid", contentDTOs[p1].uid)
+//                var activityOptions: ActivityOptions? = null
+//
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+//                    activityOptions = ActivityOptions.makeCustomAnimation(
+//                        view.context,
+//                        R.anim.fromright,
+//                        R.anim.toleft
+//                    )
+//                    Log.d("uid", "$uid")
+//                    Log.d("ui2", "${contentDTOs[p1].uid}")
+//
+//                    startActivity(intent, activityOptions.toBundle())
+//
+//                    //당근 마켓 처럼 자신의 게시글인 경우에는 채팅대화방 보기 버튼을 하고 싶은데.. 그건 아직..
+////                    if( uid == contentDTOs[p1].uid ){
+////                        Snackbar.make(view, "본인이 등록한 아이템입니다.", Snackbar.LENGTH_LONG).show()
+////                    }
+////                    else{
+////                        startActivity(intent, activityOptions.toBundle())
+////                    }
+//                }
+//            })
         }
 
 
