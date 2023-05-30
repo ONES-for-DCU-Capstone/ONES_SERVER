fef6f89c7826b4241a5c50454c6654c098d23fc1
jm <hijno@naver.com>
2023-05-30 20:30:53 +0900

    10. mapmain

--- app/src/main/java/com/example/ones_02/navigation/LocationAuthActivity.kt
+++ app/src/main/java/com/example/ones_02/navigation/LocationAuthActivity.kt
@@ -3,10 +3,12 @@ package com.example.ones_02.navigation
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.location.Location
 import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.ones_02.R
@@ -16,18 +18,29 @@ import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.GeoPoint
+import kotlinx.android.synthetic.main.frag_map.*
 
 class LocationAuthActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mapView: MapView
+    var firestore: FirebaseFirestore? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.frag_map)
+        firestore = FirebaseFirestore.getInstance()
 
         mapView = findViewById(R.id.mapView)
         mapView.onCreate(savedInstanceState)
         mapView.getMapAsync(this)
+
+        mymap_btn_back.setOnClickListener {
+            onBackPressed()
+        }
+
 
         //위치 사용 인증
         val permissions = arrayOf(
@@ -46,16 +59,68 @@ class LocationAuthActivity : AppCompatActivity(), OnMapReadyCallback {
             && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             val location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
             if (location != null) {
-                val currentLatLng = LatLng(location.latitude, location.longitude)
+//                val currentLatLng = LatLng(location.latitude, location.longitude)
+                val currentLatLng = LatLng(35.919040329022, 128.83289336918)
+
+                googleMap.addMarker(MarkerOptions().position(currentLatLng).title("여기"))
+
+                // 위도와 경도로부터 주소 가져오기 (Geocoder)
+                val geocoder = Geocoder(this)
+                val addresses = geocoder.getFromLocation(currentLatLng.latitude, currentLatLng.longitude, 1)
+
+                if (addresses.isNotEmpty()) {
+                    val address = addresses[0].getAddressLine(0)
+//                    locationdetail.text =address
+
+                    //시 일 경우
+                    //구일 경우도 있어야한다. 00구 00동일때..
+                    val startIndex = address.indexOf("시") + 2
+                    val endIndex = startIndex + 3
+                    val extractedChars = address.substring(startIndex, endIndex)
+                    locationdetail.text =extractedChars
+//                     주소를 필요에 맞게 사용하세요 (예: TextView에 표시)
+                    println(address)
+                } else {
+                    // 주소를 가져올 수 없음
+                    println("주소를 찾을 수 없음")
+                }
+
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng))
+                googleMap.moveCamera(CameraUpdateFactory.zoomTo(15f))
+
+
                 println(currentLatLng)
                 // 현재 위치 마커로 표시
                 googleMap.addMarker(MarkerOptions().position(currentLatLng).title("여기"))
                 googleMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng))
                 googleMap.moveCamera(CameraUpdateFactory.zoomTo(15f))
+
+                // 등록 버튼 클릭 시 Firestore에 위치 저장
+                locationAuth_button.setOnClickListener {
+                    saveLocationToFirestore(currentLatLng)
+                }
             }
         }
     }
 
+    //location을 DB에 등록.
+    val uid = FirebaseAuth.getInstance().currentUser?.uid
+
+    private fun saveLocationToFirestore(location: LatLng) {
+        // Firestore에 위치 데이터 저장
+        val location = GeoPoint(location.latitude, location.longitude)
+        val userRef = firestore?.collection("users")?.document(uid!!)
+        userRef?.update("location", location)
+            ?.addOnSuccessListener {
+                Toast.makeText(this@LocationAuthActivity, "저장 완료", Toast.LENGTH_SHORT).show()
+                onBackPressed()
+            }
+            ?.addOnFailureListener{ e ->
+            }
+    }
+
+
+    //생명주기 메서드
     override fun onStart() {
         super.onStart()
         mapView.onStart()
--- app/src/main/java/com/example/ones_02/navigation/MapFragment.kt
+++ app/src/main/java/com/example/ones_02/navigation/MapFragment.kt
@@ -1,20 +1,220 @@
-package com.example.ones_02.navigation
-
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.ones_02.R
+import com.example.ones_02.navigation.model.ContentDTO
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import kotlinx.android.synthetic.main.fragment_detail.view.*
+import kotlinx.android.synthetic.main.fragment_map.view.*
+import kotlinx.android.synthetic.main.item_detail.view.*
+import kotlinx.android.synthetic.main.item_mappost.view.*
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
+    var firestore : FirebaseFirestore?= null
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_map,container,false)
+    private lateinit var mapView: MapView
+    private lateinit var googleMap: GoogleMap
+    private lateinit var locationManager: LocationManager
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_map, container, false)
+
+        firestore = FirebaseFirestore.getInstance()
+
+        mapView = view.findViewById(R.id.mapViewmain)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+
+        view.mappost_recyclerveiw.adapter = MappostRecyclerViewAdapter()
+        view.mappost_recyclerveiw.layoutManager = LinearLayoutManager(activity)
+
         return view
+    }
+
+    inner class MappostRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+        var contentDTOs : ArrayList<ContentDTO> = arrayListOf()
+        var contentUidList : ArrayList<String> = arrayListOf()
+
+
+        init {
+            firestore?.collection("images")?.orderBy("timestamp",
+                Query.Direction.DESCENDING)?.addSnapshotListener{ querySnapshot, firebaseFirestoreException ->
+                contentDTOs.clear()
+                contentUidList.clear()
+
+                if(querySnapshot == null) return@addSnapshotListener
+                for(snapshot in querySnapshot!!.documents){
+                    var item = snapshot.toObject(ContentDTO::class.java)
+//
+                    contentDTOs.add(item!!)
+                    contentUidList.add(snapshot.id)
+                }
+                notifyDataSetChanged()
+            }
+        }
+
+        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
+            var view = LayoutInflater.from(p0.context).inflate(R.layout.item_mappost, p0, false)
+            return CustomViewHolder(view)
+        }
+        inner class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+
+        override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
+            var viewholder = (p0 as CustomViewHolder).itemView
+
+            //title
+            viewholder.mappost_title.text = contentDTOs!![p1].title
+            viewholder.mappost_pirce.text = contentDTOs!![p1].price
+
+
+//            val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
+//            val date = contentDTOs!![p1].timestamp?.toDate()
+//            if (date != null) {
+//                viewholder.post_feed_product_time.text = dateFormat.format(date)
+//            } else {
+//                viewholder.post_feed_product_time.text = ""
+//            }
+//
+//            val tagLayout = viewholder.post_feed_product_tag_layout
+//            val tags = contentDTOs!![p1].tag?.split(",")
+//
+//            // 각 태그마다 버튼 생성
+//            if (tags != null) {
+//                for (tag in tags) {
+//                    val tagButton = AppCompatButton(context!!)
+//
+//                    // 버튼의 글자 크기 측정
+//                    val paint = tagButton.paint
+//                    val textWidth = paint.measureText(tag)
+//
+//                    // 버튼의 크기 동적으로 설정
+//                    val params = LinearLayout.LayoutParams(
+//                        textWidth.toInt() + 40,  // 버튼의 크기 = 글자의 길이 + 여백
+//                        78
+//                    )
+//                    params.setMargins(0, 0, 10, 0) // left, top, right, bottom
+//                    tagButton.layoutParams = params
+//
+//                    tagButton.text = tag
+//                    tagButton.setBackgroundResource(R.drawable.shape_tagbox)
+//                    tagButton.setTextColor(Color.WHITE)
+//                    tagLayout.addView(tagButton)
+//                }
+//            }
+
+
+        }
+
+        override fun getItemCount(): Int {
+            return contentDTOs.size
+            println("contentDTOs.sizemap")
+            println(contentDTOs.size)
+        }
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+
+        // 위치 사용 권한 확인
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // 위치 권한이 허용된 경우
+            getCurrentLocation()
+        } else {
+            // 위치 권한 요청
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION), PERMISSION_REQUEST_CODE)
+        }
+    }
+
+    private fun getCurrentLocation() {
+        locationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        val locationListener = object : LocationListener {
+            override fun onLocationChanged(location: Location) {
+//                val currentLatLng = LatLng(location.latitude, location.longitude)
+                val currentLatLng = LatLng(35.908633261636, 128.81049581679)
+
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 13f))
+                googleMap.addMarker(MarkerOptions().position(currentLatLng).title("Current Location"))
+
+                locationManager.removeUpdates(this)
+            }
+
+            override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+            override fun onProviderEnabled(provider: String) {}
+            override fun onProviderDisabled(provider: String) {}
+        }
+
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0L, 0f, locationListener)
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    companion object {
+        private const val PERMISSION_REQUEST_CODE = 1001
     }
 }
